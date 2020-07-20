ba4cbc8d0e57fe4738f5723ce2606cdaa5ff8ef4
ARATA Mizuki <minorinoki@gmail.com>
2020-07-20 21:37:20 +0900

    Move remainder to it's own module

--- fp-ieee/src/Numeric/Floating/IEEE.hs
+++ fp-ieee/src/Numeric/Floating/IEEE.hs
@@ -168,27 +168,3 @@ module Numeric.Floating.IEEE
   ) where
 import           MyPrelude
 import           Numeric.Floating.IEEE.Internal
-
-default ()
-
--- |
--- IEEE 754 @remainder@ operation.
-remainder :: RealFloat a => a -> a -> a
-remainder x y | isFinite x && isInfinite y = x
-              | y == 0 || isInfinite y || isNaN y || not (isFinite x) = (x - x) / y * y -- return a NaN
-              | otherwise = let n = round (toRational x / toRational y) -- TODO: Is round (x / y) okay?
-                                r = x - y * fromInteger n
-                            in r -- if r == 0, the sign of r is the same as x
-{-# NOINLINE [1] remainder #-}
-
-#ifdef USE_FFI
-foreign import ccall unsafe "remainderf"
-  c_remainderFloat :: Float -> Float -> Float
-foreign import ccall unsafe "remainder"
-  c_remainderDouble :: Double -> Double -> Double
-
-{-# RULES
-"remainder/Float" remainder = c_remainderFloat
-"remainder/Double" remainder = c_remainderDouble
-  #-}
-#endif
--- fp-ieee/src/Numeric/Floating/IEEE/Internal.hs
+++ fp-ieee/src/Numeric/Floating/IEEE/Internal.hs
@@ -2,13 +2,16 @@
 {-# OPTIONS_HADDOCK not-home #-}
 module Numeric.Floating.IEEE.Internal (module Internal) where
 import           Numeric.Floating.IEEE.Internal.Augmented as Internal
-import           Numeric.Floating.IEEE.Internal.Base as Internal hiding ((^!), staticIf)
+import           Numeric.Floating.IEEE.Internal.Base as Internal hiding
+                                                                  (staticIf,
+                                                                  (^!))
 import           Numeric.Floating.IEEE.Internal.Classify as Internal
 import           Numeric.Floating.IEEE.Internal.Conversion as Internal
 import           Numeric.Floating.IEEE.Internal.FMA as Internal
 import           Numeric.Floating.IEEE.Internal.GenericArith as Internal
 import           Numeric.Floating.IEEE.Internal.MinMax as Internal
 import           Numeric.Floating.IEEE.Internal.NextFloat as Internal
+import           Numeric.Floating.IEEE.Internal.Remainder as Internal
 import           Numeric.Floating.IEEE.Internal.Rounding as Internal
 import           Numeric.Floating.IEEE.Internal.RoundToIntegral as Internal
 #if defined(USE_HALF)
