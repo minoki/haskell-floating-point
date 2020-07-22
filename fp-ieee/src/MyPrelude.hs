50f2b3c67007727cb2eaaf808bea092337b1f878
ARATA Mizuki <minorinoki@gmail.com>
2020-07-22 17:27:32 +0900

    Add comment for MyPrelude

--- fp-ieee/src/MyPrelude.hs
+++ fp-ieee/src/MyPrelude.hs
@@ -1,2 +1,14 @@
+{-
+This module is the custom Prelude for this project.
+You can replace Prelude's definition by a debugging-friendly one.
+Examples are:
+
+type RealFloat a = (Prelude.RealFloat a, Show a)
+
+(^) :: (HasCallStack, Num a, Integral b) => a -> b -> a
+x ^ y | y < 0 = error "Negative exponent" -- with stack trace
+      | otherwise = x Prelude.^ y
+-}
+
 module MyPrelude (module Prelude) where
 import Prelude
