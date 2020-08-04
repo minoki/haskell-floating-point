d744b4d560b0f10c88a960aeceb19c97b8390705
ARATA Mizuki <minorinoki@gmail.com>
2020-08-04 12:06:19 +0900

    rounded-hw/test: Update FromIntegerSpec

--- rounded-hw/test/FromIntegerSpec.hs
+++ rounded-hw/test/FromIntegerSpec.hs
@@ -19,32 +19,32 @@ prop_fromInteger_nearest_stock _proxy x
 prop_roundedFromInteger_check :: forall a. (RealFloat a, RoundedRing a) => Proxy a -> RoundingMode -> Integer -> Property
 prop_roundedFromInteger_check _proxy r x
   = (roundedFromInteger r x :: a)
-    `sameFloatP` (fromInt r x :: a)
+    `sameFloatP` (roundedFromInteger_default r x :: a)
 
 prop_roundedFromInt64_check :: forall a. (RealFloat a, RoundedRing a) => Proxy a -> RoundingMode -> Int64 -> Property
 prop_roundedFromInt64_check _proxy r x
   = (roundedFromInteger r (fromIntegral x) :: a)
-    `sameFloatP` (fromInt r (fromIntegral x) :: a)
+    `sameFloatP` (roundedFromInteger_default r (fromIntegral x) :: a)
 
 prop_roundedFromWord64_check :: forall a. (RealFloat a, RoundedRing a) => Proxy a -> RoundingMode -> Word64 -> Property
 prop_roundedFromWord64_check _proxy r x
   = (roundedFromInteger r (fromIntegral x) :: a)
-    `sameFloatP` (fromInt r (fromIntegral x) :: a)
+    `sameFloatP` (roundedFromInteger_default r (fromIntegral x) :: a)
 
-prop_fromInt_order :: forall a. RealFloat a => Proxy a -> Integer -> Property
-prop_fromInt_order _proxy x
-  = let ne   = fromInt ToNearest    x :: a
-        ze   = fromInt TowardZero   x :: a
-        inf  = fromInt TowardInf    x :: a
-        ninf = fromInt TowardNegInf x :: a
+prop_roundedFromInteger_order :: forall a. (RealFloat a, RoundedRing a) => Proxy a -> Integer -> Property
+prop_roundedFromInteger_order _proxy x
+  = let ne   = roundedFromInteger ToNearest    x :: a
+        ze   = roundedFromInteger TowardZero   x :: a
+        inf  = roundedFromInteger TowardInf    x :: a
+        ninf = roundedFromInteger TowardNegInf x :: a
     in ninf <= inf
        .&&. (ne == ninf || ne == inf)
        .&&. (if x < 0 then ze == inf else ze == ninf)
 
-prop_fromInt_exact :: forall a. RealFloat a => Proxy a -> Integer -> Property
-prop_fromInt_exact _proxy x
-  = let inf  = fromInt TowardInf    x :: a
-        ninf = fromInt TowardNegInf x :: a
+prop_roundedFromInteger_exact :: forall a. (RealFloat a, RoundedRing a) => Proxy a -> Integer -> Property
+prop_roundedFromInteger_exact _proxy x
+  = let inf  = roundedFromInteger TowardInf    x :: a
+        ninf = roundedFromInteger TowardNegInf x :: a
     in if ninf == inf
        then not (isInfinite inf) .&&. toRational inf === fromInteger x
        else if isInfinite inf
@@ -71,9 +71,9 @@ specT proxy checkAgainstStock = do
   prop "roundedFromInteger/Word64" $ \r ->
     prop_roundedFromWord64_check proxy r
   prop "order" $
-    forAllShrink variousIntegers shrinkIntegral (prop_fromInt_order proxy)
+    forAllShrink variousIntegers shrinkIntegral (prop_roundedFromInteger_order proxy)
   prop "exactness" $
-    forAllShrink variousIntegers shrinkIntegral (prop_fromInt_exact proxy)
+    forAllShrink variousIntegers shrinkIntegral (prop_roundedFromInteger_exact proxy)
 
 spec :: Spec
 spec = do
