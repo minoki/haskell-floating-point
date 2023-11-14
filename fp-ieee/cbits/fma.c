539a3c6b22e0ee19c3e299c5fdf2b74d8ed6857a
ARATA Mizuki <minorinoki@gmail.com>
2023-11-14 18:30:12 +0900

    fp-ieee: Make sure FMA instruction is used even if GCC's optimizations are disabled

--- fp-ieee/cbits/fma.c
+++ fp-ieee/cbits/fma.c
@@ -1,5 +1,26 @@
 #include <math.h>
 
+#if defined(__GNUC__) && defined(__FMA__)
+
+// Make sure FMA instruction is used even if optimizations are disabled.
+
+double hs_fusedMultiplyAddDouble(double a, double b, double c)
+{
+    // vfmadd132sd %xmm1, %xmm2, %xmm0
+    // %xmm0 <- %xmm0 * %xmm1 + %xmm2
+    __asm__("vfmadd132sd %1, %2, %0" : "+x"(a) : "x"(b), "x"(c));
+    return a;
+}
+float hs_fusedMultiplyAddFloat(float a, float b, float c)
+{
+    // vfmadd132ss %xmm1, %xmm2, %xmm0
+    // %xmm0 <- %xmm0 * %xmm1 + %xmm2
+    __asm__("vfmadd132ss %1, %2, %0" : "+x"(a) : "x"(b), "x"(c));
+    return a;
+}
+
+#else
+
 #if !defined(FP_FAST_FMA)
 #error "The compiler should define FP_FAST_FMA"
 #endif
@@ -15,3 +36,5 @@ float hs_fusedMultiplyAddFloat(float a, float b, float c)
 {
     return fmaf(a, b, c);
 }
+
+#endif
