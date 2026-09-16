8b1dc89f5ce3af89db60f36ff95d8497920d2235
ARATA Mizuki <minorinoki@gmail.com>
2026-09-16 13:18:02 +0900

    rounded-hw: Fix distanceUlp on [-maxFinite, maxFinite]

--- rounded-hw/src/Numeric/Rounded/Hardware/Internal/FloatUtil.hs
+++ rounded-hw/src/Numeric/Rounded/Hardware/Internal/FloatUtil.hs
@@ -14,8 +14,9 @@ distanceUlp :: RealFloat a => a -> a -> Maybe Integer
 distanceUlp x y
   | isInfinite x || isInfinite y || isNaN x || isNaN y = Nothing
   | otherwise = let m = min (abs x) (abs y)
-                    m' = nextUp m
-                    v = (toRational y - toRational x) / toRational (m' - m)
+                    ulp | m == 0 = minPositive
+                        | otherwise = encodeFloat 1 (max (fst (floatRange m)) (exponent m) - floatDigits m) `asTypeOf` m
+                    v = (toRational y - toRational x) / toRational ulp
                 in if denominator v == 1
                    then Just (abs (numerator v))
                    else error "distanceUlp"
